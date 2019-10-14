290779ef192b639c3212362bb3b40a11e4af3eb6
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-10-14 12:38:21 +0900

    IDD_WEBCAMERA

--- resource.h
+++ resource.h
@@ -13,6 +13,7 @@
 #define IDD_MAIN                            100
 #define IDD_SOUNDINPUT                      101
 #define IDD_SCREEN                          102
+#define IDD_WEBCAMERA                       103
 
 #define IDR_SILENT_WAV                      1
 #define IDR_MAINMENU                        1000
